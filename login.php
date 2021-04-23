b3b35e3c40217acc0b73d55757d88c7d23d44316
Njoli Patrick <ogmaro@gmail.com>
2021-04-23 19:26:12 +0100

    added feature for login

--- login.php
+++ login.php
@@ -3,21 +3,27 @@
 
 <head>
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
 
-    <form method="post">
+    <form action="auth.php" method="post">
         <div class="container">
             <h1>Zuri - Login</h1>
             <p>Sign in your account here</p>
             <hr>
+            <?php if (isset($_GET['error'])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= htmlspecialchars($_GET['error']) ?>
+                </div>
+            <?php } ?>
             <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="email" id="email" required>
+            <input type="text" placeholder="Enter Email" name="email" value="<?php if (isset($_GET['email'])) echo (htmlspecialchars($_GET['email'])) ?>" id="email">
 
             <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+            <input type="password" placeholder="Enter Password" name="psw" id="psw">
 
             <button type="submit" name="login" class="registerbtn">Login</button>
         </div>
--- login_success.php
+++ login_success.php
@@ -1,6 +1,12 @@
+<?php
+session_start();
+
+?>
+
 <!DOCTYPE html>
 
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="css/success.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
 </head>
@@ -11,6 +17,9 @@
             <i class="checkmark">✓</i>
         </div>
         <h1>Login Successful</h1>
+        <h2><?php echo $_SESSION['full_name'] ?></h2>
+        <h2><?php echo $_SESSION['email'] ?></h2>
+        <h2><?php echo $_SESSION['phone'] ?></h2>
     </div>
     <div class="container signin">
         <p>Don't have an account? <a href="logout.php">logout</a>.</p>
